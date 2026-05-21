ccc04619560a029e822001f5d5f968f84a416de7
mvitoria369 <fernandes.silva.maria2508@escola.pr.gov.br>
2026-05-21 10:47:48 +0000

    ...

--- main.js
+++ main.js
@@ -28,3 +28,8 @@ function showOffsetPos(sId) {
 }
 
 showOffsetPos("content");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
